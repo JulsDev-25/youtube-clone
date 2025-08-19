fe6deb9badc2c719639995128443c1eac7e500e6
Jules <julesloic9@gmail.com>
2025-08-19 11:17:48 +0200

    mui installation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Main from './layourts/main'
 
 function App() {
 
   return (
     <>
-      <h1>Message de bienvenue</h1>
+      <Main/>
     </>
   )
 }
---
+++ src/layourts/main/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Main = () => {
+    return (
+        <>
+            <p>Bonjour</p>
+        </>
+    )
+}
+
+export default Main
