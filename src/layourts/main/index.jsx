d9d50cf58854cfe8f334a8a7c2b02e354f43107f
Jules <julesloic9@gmail.com>
2025-09-02 15:27:15 +0200

    updates for adding components

--- src/layourts/main/index.jsx
+++ src/layourts/main/index.jsx
@@ -1,10 +1,60 @@
-import React from "react";
+import React, { use } from "react";
+import NavBar from "../../components/NavBar";
+import { Box, CssBaseline, Typography, useTheme } from "@mui/material";
+import SideBar from "../../components/SideBar";
+import TabFilter from "../../components/TabFilter";
 
 const Main = () => {
+    const [openDrawer, setOpenDrawer] = React.useState(false);
+
+    const theme = useTheme();
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
     return (
-        <>
-            <p>Bonjour</p>
-        </>
+        <Box sx={{flex:1 }}>
+            <CssBaseline />
+            <NavBar setOpenDrawer={setOpenDrawer} openDrawer={openDrawer} />
+            <Box sx={{ display: 'flex' }}>
+                <SideBar open={openDrawer} sx={{ mt: theme.mixins.toolbar.minHeight }} />
+                <Box component="main" sx={{ flexGrow: 1, px: 3, overflow: "hidden" }}>
+                  <TabFilter />
+                    <Typography sx={{ marginBottom: 2 }}>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
+                    enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
+                    imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
+                    Convallis convallis tellus id interdum velit laoreet id donec ultrices.
+                    Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
+                    adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra
+                    nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum
+                    leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis
+                    feugiat vivamus at augue. At augue eget arcu dictum varius duis at
+                    consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
+                    sapien faucibus et molestie ac.
+                    </Typography>
+                    <Typography sx={{ marginBottom: 2 }}>
+                    Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
+                    eget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enim
+                    neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra
+                    tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis
+                    sed odio morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi
+                    tincidunt ornare massa eget egestas purus viverra accumsan in. In hendrerit
+                    gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
+                    et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis
+                    tristique sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
+                    eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
+                    posuere sollicitudin aliquam ultrices sagittis orci a.
+                    </Typography>
+                </Box>
+            </Box>
+        </Box>
     )
 }
 
