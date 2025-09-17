bc5835b085778a6c145b9c421cb862d404748286
Jules <julesloic9@gmail.com>
2025-09-17 12:43:52 +0200

    Add video cards and make them responsive, update the styles to all components responsible

--- src/layourts/main/index.jsx
+++ src/layourts/main/index.jsx
@@ -1,57 +1,305 @@
-import React, { use } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import NavBar from "../../components/NavBar";
-import { Box, CssBaseline, Typography, useTheme } from "@mui/material";
+import { Box, CssBaseline, Grid, useTheme } from "@mui/material";
 import SideBar from "../../components/SideBar";
 import TabFilter from "../../components/TabFilter";
+import VideoCard from "../../components/VideoCard";
+import miniature from '../../assets/img/hq720.webp';
+import authPhoto from '../../assets/img/hq720.webp';
 
 const Main = () => {
-    const [openDrawer, setOpenDrawer] = React.useState(false);
+    const sideBarRef = useRef(null)
+    const [openDrawer, setOpenDrawer] = useState(false);
+    const [sideBarwidth, setSideBarWidth] = useState(68)
+
+    useEffect(() => {
+      if (sideBarRef.current) {
+        console.log("Sidebar ref:", sideBarRef.current.offsetWidth);
+        setSideBarWidth(sideBarRef.current.offsetWidth)
+      }
+    }, [openDrawer])
+    
 
     const theme = useTheme();
 
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
-
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
+    const Videos = [
+      {
+        miniature: miniature,
+        titre: "The Day Amad Diallo SHOCKED Ruben Amorim & The World",
+        description:
+          "Amad Diallo scored a sensational solo goal to help Manchester United Under-23s to a 3-1 win over Sporting Lisbon in the UEFA Youth League.",
+        auteur: {
+          nom: "11Edit",
+          photo: authPhoto,
+        },
+        durée: "12:34",
+        vues: "1.2M",
+        date: "3 jours",
+      },
+      {
+        miniature: miniature,
+        titre: "The Day Amad Diallo SHOCKED Ruben Amorim & The World",
+        description:
+          "Amad Diallo scored a sensational solo goal to help Manchester United Under-23s to a 3-1 win over Sporting Lisbon in the UEFA Youth League.",
+        auteur: {
+          nom: "11Edit",
+          photo: authPhoto,
+        },
+        durée: "12:34",
+        vues: "1.2M",
+        date: "3 jours",
+      },
+      {
+        miniature: miniature,
+        titre: "The Day Amad Diallo SHOCKED Ruben Amorim & The World",
+        description:
+          "Amad Diallo scored a sensational solo goal to help Manchester United Under-23s to a 3-1 win over Sporting Lisbon in the UEFA Youth League.",
+        auteur: {
+          nom: "11Edit",
+          photo: authPhoto,
+        },
+        durée: "12:34",
+        vues: "1.2M",
+        date: "3 jours",
+      },
+      {
+        miniature: miniature,
+        titre: "The Day Amad Diallo SHOCKED Ruben Amorim & The World",
+        description:
+          "Amad Diallo scored a sensational solo goal to help Manchester United Under-23s to a 3-1 win over Sporting Lisbon in the UEFA Youth League.",
+        auteur: {
+          nom: "11Edit",
+          photo: authPhoto,
+        },
+        durée: "12:34",
+        vues: "1.2M",
+        date: "3 jours",
+      },
+      {
+        miniature: miniature,
+        titre: "The Day Amad Diallo SHOCKED Ruben Amorim & The World",
+        description:
+          "Amad Diallo scored a sensational solo goal to help Manchester United Under-23s to a 3-1 win over Sporting Lisbon in the UEFA Youth League.",
+        auteur: {
+          nom: "11Edit",
+          photo: authPhoto,
+        },
+        durée: "12:34",
+        vues: "1.2M",
+        date: "3 jours",
+      },
+      {
+        miniature: miniature,
+        titre: "The Day Amad Diallo SHOCKED Ruben Amorim & The World",
+        description:
+          "Amad Diallo scored a sensational solo goal to help Manchester United Under-23s to a 3-1 win over Sporting Lisbon in the UEFA Youth League.",
+        auteur: {
+          nom: "11Edit",
+          photo: authPhoto,
+        },
+        durée: "12:34",
+        vues: "1.2M",
+        date: "3 jours",
+      },
+      {
+        miniature: miniature,
+        titre: "The Day Amad Diallo SHOCKED Ruben Amorim & The World",
+        description:
+          "Amad Diallo scored a sensational solo goal to help Manchester United Under-23s to a 3-1 win over Sporting Lisbon in the UEFA Youth League.",
+        auteur: {
+          nom: "11Edit",
+          photo: authPhoto,
+        },
+        durée: "12:34",
+        vues: "1.2M",
+        date: "3 jours",
+      },
+      {
+        miniature: miniature,
+        titre: "The Day Amad Diallo SHOCKED Ruben Amorim & The World",
+        description:
+          "Amad Diallo scored a sensational solo goal to help Manchester United Under-23s to a 3-1 win over Sporting Lisbon in the UEFA Youth League.",
+        auteur: {
+          nom: "11Edit",
+          photo: authPhoto,
+        },
+        durée: "12:34",
+        vues: "1.2M",
+        date: "3 jours",
+      },
+      {
+        miniature: miniature,
+        titre: "The Day Amad Diallo SHOCKED Ruben Amorim & The World",
+        description:
+          "Amad Diallo scored a sensational solo goal to help Manchester United Under-23s to a 3-1 win over Sporting Lisbon in the UEFA Youth League.",
+        auteur: {
+          nom: "11Edit",
+          photo: authPhoto,
+        },
+        durée: "12:34",
+        vues: "1.2M",
+        date: "3 jours",
+      },
+      {
+        miniature: miniature,
+        titre: "The Day Amad Diallo SHOCKED Ruben Amorim & The World",
+        description:
+          "Amad Diallo scored a sensational solo goal to help Manchester United Under-23s to a 3-1 win over Sporting Lisbon in the UEFA Youth League.",
+        auteur: {
+          nom: "11Edit",
+          photo: authPhoto,
+        },
+        durée: "12:34",
+        vues: "1.2M",
+        date: "3 jours",
+      },
+      {
+        miniature: miniature,
+        titre: "The Day Amad Diallo SHOCKED Ruben Amorim & The World",
+        description:
+          "Amad Diallo scored a sensational solo goal to help Manchester United Under-23s to a 3-1 win over Sporting Lisbon in the UEFA Youth League.",
+        auteur: {
+          nom: "11Edit",
+          photo: authPhoto,
+        },
+        durée: "12:34",
+        vues: "1.2M",
+        date: "3 jours",
+      },
+      {
+        miniature: miniature,
+        titre: "The Day Amad Diallo SHOCKED Ruben Amorim & The World",
+        description:
+          "Amad Diallo scored a sensational solo goal to help Manchester United Under-23s to a 3-1 win over Sporting Lisbon in the UEFA Youth League.",
+        auteur: {
+          nom: "11Edit",
+          photo: authPhoto,
+        },
+        durée: "12:34",
+        vues: "1.2M",
+        date: "3 jours",
+      },
+      {
+        miniature: miniature,
+        titre: "The Day Amad Diallo SHOCKED Ruben Amorim & The World",
+        description:
+          "Amad Diallo scored a sensational solo goal to help Manchester United Under-23s to a 3-1 win over Sporting Lisbon in the UEFA Youth League.",
+        auteur: {
+          nom: "11Edit",
+          photo: authPhoto,
+        },
+        durée: "12:34",
+        vues: "1.2M",
+        date: "3 jours",
+      },
+      {
+        miniature: miniature,
+        titre: "The Day Amad Diallo SHOCKED Ruben Amorim & The World",
+        description:
+          "Amad Diallo scored a sensational solo goal to help Manchester United Under-23s to a 3-1 win over Sporting Lisbon in the UEFA Youth League.",
+        auteur: {
+          nom: "11Edit",
+          photo: authPhoto,
+        },
+        durée: "12:34",
+        vues: "1.2M",
+        date: "3 jours",
+      },
+      {
+        miniature: miniature,
+        titre: "The Day Amad Diallo SHOCKED Ruben Amorim & The World",
+        description:
+          "Amad Diallo scored a sensational solo goal to help Manchester United Under-23s to a 3-1 win over Sporting Lisbon in the UEFA Youth League.",
+        auteur: {
+          nom: "11Edit",
+          photo: authPhoto,
+        },
+        durée: "12:34",
+        vues: "1.2M",
+        date: "3 jours",
+      },
+      {
+        miniature: miniature,
+        titre: "The Day Amad Diallo SHOCKED Ruben Amorim & The World",
+        description:
+          "Amad Diallo scored a sensational solo goal to help Manchester United Under-23s to a 3-1 win over Sporting Lisbon in the UEFA Youth League.",
+        auteur: {
+          nom: "11Edit",
+          photo: authPhoto,
+        },
+        durée: "12:34",
+        vues: "1.2M",
+        date: "3 jours",
+      },
+      {
+        miniature: miniature,
+        titre: "The Day Amad Diallo SHOCKED Ruben Amorim & The World",
+        description:
+          "Amad Diallo scored a sensational solo goal to help Manchester United Under-23s to a 3-1 win over Sporting Lisbon in the UEFA Youth League.",
+        auteur: {
+          nom: "11Edit",
+          photo: authPhoto,
+        },
+        durée: "12:34",
+        vues: "1.2M",
+        date: "3 jours",
+      },
+      {
+        miniature: miniature,
+        titre: "The Day Amad Diallo SHOCKED Ruben Amorim & The World",
+        description:
+          "Amad Diallo scored a sensational solo goal to help Manchester United Under-23s to a 3-1 win over Sporting Lisbon in the UEFA Youth League.",
+        auteur: {
+          nom: "11Edit",
+          photo: authPhoto,
+        },
+        durée: "12:34",
+        vues: "1.2M",
+        date: "3 jours",
+      },
+      {
+        miniature: miniature,
+        titre: "The Day Amad Diallo SHOCKED Ruben Amorim & The World",
+        description:
+          "Amad Diallo scored a sensational solo goal to help Manchester United Under-23s to a 3-1 win over Sporting Lisbon in the UEFA Youth League.",
+        auteur: {
+          nom: "11Edit",
+          photo: authPhoto,
+        },
+        durée: "12:34",
+        vues: "1.2M",
+        date: "3 jours",
+      },
+    ];
 
     return (
-        <Box sx={{flex:1 }}>
+        <Box sx={{flex:1, position: "relative"}}>
             <CssBaseline />
             <NavBar setOpenDrawer={setOpenDrawer} openDrawer={openDrawer} />
-            <Box sx={{ display: 'flex' }}>
-                <SideBar open={openDrawer} sx={{ mt: theme.mixins.toolbar.minHeight }} />
+            <Box sx={{ display: 'flex', }}>
+                <SideBar open={openDrawer} sideBarRef={sideBarRef} />
                 <Box component="main" sx={{ flexGrow: 1, px: 3, overflow: "hidden" }}>
-                  <TabFilter />
-                    <Typography sx={{ marginBottom: 2 }}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
-                    enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
-                    imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
-                    Convallis convallis tellus id interdum velit laoreet id donec ultrices.
-                    Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-                    adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra
-                    nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum
-                    leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis
-                    feugiat vivamus at augue. At augue eget arcu dictum varius duis at
-                    consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
-                    sapien faucibus et molestie ac.
-                    </Typography>
-                    <Typography sx={{ marginBottom: 2 }}>
-                    Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
-                    eget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enim
-                    neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra
-                    tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis
-                    sed odio morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi
-                    tincidunt ornare massa eget egestas purus viverra accumsan in. In hendrerit
-                    gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
-                    et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis
-                    tristique sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-                    eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-                    posuere sollicitudin aliquam ultrices sagittis orci a.
-                    </Typography>
+                  <Box sx={{width: `calc(100% - ${sideBarwidth}px - 48px)`, position: 'fixed', top: '64px', backgroundColor: '#fff'}} >
+                    <TabFilter />
+                  </Box>
+                  
+                  {/* First section video card */}
+                  <Grid container spacing={2}>
+                    {Videos.map((video, index) => (
+                      <VideoCard 
+                        key={index}
+                        miniature={video.miniature}
+                        titre={video.titre}
+                        description={video.description}
+                        auteur={video.auteur}
+                        durée={video.durée}
+                        vues={video.vues}
+                        date={video.date}
+                      />
+                    ))}
+                  </Grid>
+
+                  {/* Second section video card */}
                 </Box>
             </Box>
         </Box>
