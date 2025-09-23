d03d501a2a41f78a1a04c7aed5a1190cf53489fe
Jules <julesloic9@gmail.com>
2025-09-23 21:02:32 +0200

    add shotCard component and improve responsive

--- src/layourts/main/index.jsx
+++ src/layourts/main/index.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useRef, useState } from "react";
 import NavBar from "../../components/NavBar";
-import { Box, CssBaseline, Grid, useTheme } from "@mui/material";
+import { Box, CssBaseline, Grid, Typography, useTheme } from "@mui/material";
 import SideBar from "../../components/SideBar";
 import TabFilter from "../../components/TabFilter";
 import VideoCard from "../../components/VideoCard";
 import miniature from '../../assets/img/hq720.webp';
 import authPhoto from '../../assets/img/hq720.webp';
+import ShotCard from "../../components/ShotCard";
 
 const Main = () => {
     const sideBarRef = useRef(null)
@@ -274,16 +275,51 @@ const Main = () => {
 
     return (
         <Box sx={{flex:1, position: "relative"}}>
+
             <CssBaseline />
+
             <NavBar setOpenDrawer={setOpenDrawer} openDrawer={openDrawer} />
+
             <Box sx={{ display: 'flex', }}>
                 <SideBar open={openDrawer} sideBarRef={sideBarRef} />
+
                 <Box component="main" sx={{ flexGrow: 1, px: 3, overflow: "hidden" }}>
-                  <Box sx={{width: `calc(100% - ${sideBarwidth}px - 48px)`, position: 'fixed', top: '64px', backgroundColor: '#fffc', backdropFilter: 'blur(10px)'}} >
+                  <Box sx={{width: {sm: `calc(100% - 48px)`, lg: `calc(100% - ${sideBarwidth}px - 48px)`}, position: 'fixed', top: '64px', backgroundColor: '#fffc', backdropFilter: 'blur(10px)'}} >
                     <TabFilter />
                   </Box>
                   
                   {/* First section video card */}
+                  <Box sx={{ mt: {xs: '64px', sm: '0px'} }}></Box>
+                  <Grid container spacing={2} sx={{ mt: '64px', flexWrap: 'nowrap', display: {xs: 'none', sm: 'flex'} }}>
+                    {Videos.slice(0,2).map((video, index) => (
+                      <VideoCard 
+                        key={index}
+                        miniature={video.miniature}
+                        titre={video.titre}
+                        description={video.description}
+                        auteur={video.auteur}
+                        durée={video.durée}
+                        vues={video.vues}
+                        date={video.date}
+                      />
+                    ))}
+                  </Grid>
+
+                  {/* Second section shot card */}
+                  <Box section='second-section' >
+                    {/* Shot card section */}
+                    <Typography variant="h6" fontWeight={600} sx={{ mt: 4, mb: 2 }}>
+                      Shorts
+                    </Typography>
+
+                    <Grid container spacing={2} sx={{ flexWrap: 'nowrap'}}>
+                      {Videos.slice(0,3).map((video, index) => (
+                        <ShotCard key={index} />
+                      ))}
+                    </Grid>
+                  </Box>
+
+                  {/* thirdth section video card */}
                   <Grid container spacing={2} sx={{ mt: '50px'}}>
                     {Videos.map((video, index) => (
                       <VideoCard 
